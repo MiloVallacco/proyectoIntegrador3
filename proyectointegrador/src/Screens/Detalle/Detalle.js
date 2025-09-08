f55309da26d62d5e8b9db073063cd414ca3672c9
MiloVallacco <Kmilovallacco@gmail.com>
2025-09-08 11:18:57 -0300

    Arme la base, cree las rutas y les di estado. Por ultimo tabmien arme el not found

---
+++ proyectointegrador/src/Screens/Detalle/Detalle.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+
+// Aca abajo creamos un componente con estado
+class Detalle extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+
+    }
+
+    render(){
+        return(
+        <p>Detalle</p>
+        )
+    }
+
+}
+
+export default Detalle;
---
+++ proyectointegrador/src/Screens/Home/Home.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+
+// Aca abajo creamos un componente con estado
+class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+
+    }
+
+    render(){
+        return(
+        <p>Home</p>
+        )
+    }
+
+}
+
+export default Home;
