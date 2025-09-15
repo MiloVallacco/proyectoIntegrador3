8331d4cf632411e9bbf51fd920f89cca8557e070
justop1 <jpresaras@udesa.edu.ar>
2025-09-15 12:44:53 -0300

    pagina del detalle completa

--- proyectointegrador/src/Screens/Detalle/Detalle.js
+++ proyectointegrador/src/Screens/Detalle/Detalle.js
@@ -1,22 +1,47 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
-// Aca abajo creamos un componente con estado
 class Detalle extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-
-        }
-
+        this.state = {
+            pelicula: [],
+            loading: true,
+        };
     }
 
-    render(){
-        return(
-        <p>Detalle</p>
-        )
+    componentDidMount() {
+        fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=541bfa5aac0dd7cf0e6ec5eaec4a926b`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                this.setState({ 
+                    pelicula: data, 
+                    loading: false 
+                });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                this.setState({ loading: false });
+            });
+    } 
+    
+    render() {
+        return (
+            <React.Fragment>
+                <main>
+                    <h2 className="titulo">{this.state.pelicula.title}</h2>
+                    <img  className="portada"
+                        src={`https://image.tmdb.org/t/p/w500${this.state.pelicula.poster_path}`} alt={this.state.pelicula.title}  
+                    />
+                    <p className="rating">Rating: {this.state.pelicula.vote_average}</p>
+                    <p className="fechaEstreno">Fecha de estreno: {this.state.pelicula.release_date}</p>
+                    <p className="duración">Duración: {this.state.pelicula.runtime} </p>
+                    <p className="sinopsis">Sinópsis: {this.state.pelicula.overview}</p>
+                    <p className="genero">Género: {this.state.pelicula.genres?.map(genre => genre.name)}</p>                
+                    </main>
+            </React.Fragment>
+        );
     }
-
 }
 
 export default Detalle;
